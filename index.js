048e3e2a9d3dbe4a85090e2c687fa888e5da85a0
CPA Coin CryptoPayAfrica <40791538+devafrica@users.noreply.github.com>
2020-09-23 22:51:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ client.on('message', msg => {
     curl.setHeaders([
       'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
     ])
-    .get('http://api.cryptopay.org.za/q/hashrate/')
+    .get('https://blocks.cryptopay.org.za/hashrate/')
     .then(({statusCode, body, headers}) => {
       msg.reply(body)
     })
